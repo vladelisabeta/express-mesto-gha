30a95950feeb1f6e0b967f1c5fb0255f703c14a3
Elisaveta Vlad <vladelisabeta@yandex.ru>
2023-09-23 17:40:49 +0300

    error 404

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const {
+  HTTP_STATUS_NOT_FOUND,
+} = require('http2').constants;
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
-const errorRouter = require('./routes/errorRoute');
-// const NotFoundError = require('./errors/notFoundError');
 
 const { PORT = 3000, MONGOHOST = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -28,7 +29,11 @@ app.use((req, res, next) => {
 
 app.use(userRouter);
 app.use(cardRouter);
-app.use(errorRouter);
+// app.use(errorRouter);
+
+app.use('*', (req, res) => {
+  res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Такой страницы не существует' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- routes/errorRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const NotFoundError = require('../errors/notFoundError');
-
-router.use('*', (req, res, next) => {
-  next(new NotFoundError('Такой страницы не существует.'));
-});
-
-module.exports = router;
