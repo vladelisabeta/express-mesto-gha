ed5152f1bfb2ebdb817c61f491b2c2f35df06511
Elisaveta Vlad <vladelisabeta@yandex.ru>
2023-10-21 21:31:41 +0300

    fixed regex

--- models/user.js
+++ models/user.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator(link) {
-        return /^https?:\/\/(www\.)?[\w-._~:/?#[\]@!$&'()*+,;=]*#?/.test(link);
+        return /(^https?:\/\/)?([\\da-z\\.-]+)\.([a-z\\.]{2,6})([\\/\\w\\.-]*)*\/?/.test(link);
       },
     },
   },
